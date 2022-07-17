f3c0d15ecf857671b4534a2a20282f92a0b5bc48
Laura Ross <lauramarie100@gmail.com>
2022-07-16 23:59:24 -0400

    Analysis Output
    Completed challenge analysis, moved files that were used to create the initial database into a sub-folder within Data titled "Database_Start_Files" to keep distinct from files created through the analysis process.

---
+++ Employee_Database_challenge.sql
@@ -0,0 +1,98 @@
+-- Deliverable 1
+    -- Retrieve the emp_no, first_name, and last_name columns from the Employees table.
+    -- Retrieve the title, from_date, and to_date columns from the Titles table.
+    -- Create a new table using the INTO clause.
+    -- Join both tables on the primary key.
+    -- Filter the data on the birth_date column to retrieve the employees who were born between 1952 and 1955.
+    -- Order by the employee number. * Also order descending by to-date so newest jobs show first.
+    -- Export the Retirement Titles table from the previous step as retirement_titles.csv
+        --save it to the Data folder in the Pewlett-Hackard-Analysis folder.
+
+SELECT e.emp_no, e.first_name, e.last_name, t.title, t.from_date, t.to_date
+INTO retirement_titles
+FROM employees AS e
+INNER JOIN titles AS t
+    ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no, t.to_date DESC;
+
+-- Check table
+SELECT * FROM retirement_titles
+---------------------------------------------------------------------------
+-- Retrieve the following columns from the retirment titles table:
+    -- Employee number
+    -- first name
+    -- Last name
+    -- Title
+
+-- Use the DISTINCT ON statement to retrieve the first occurrence of the employee number
+    -- for each set of rows defined by the ON () clause.
+-- Create a Unique Titles table using the INTO clause.
+-- Exclude those employees that have already left the company by filtering on to_date 
+    -- to keep only those dates that are equal to '9999-01-01'.
+-- Sort the Unique Titles table in ascending order by the employee number
+    -- Ensure to-date sorted descending so newest title is first (NOTE: this was done above when creating initial retirement_titles table).
+
+SELECT DISTINCT ON(emp_no) emp_no, first_name, last_name, title
+INTO unique_titles
+FROM retirement_titles
+WHERE (to_date = '9999-01-01')
+ORDER BY emp_no;
+
+-- Check table
+SELECT * FROM unique_titles;
+
+-- Export the Unique Titles table as unique_titles.csv and save it to your Data folder
+    -- in the Pewlett-Hackard-Analysis folder.
+
+---------------------------------------------------------------------------
+-- Write another query in the Employee_Database_challenge.sql file
+    -- to retrieve the number of employees by their most recent job title who are about to retire.
+-- First, retrieve the number of titles from the Unique Titles table.
+-- Then, create a Retiring Titles table to hold the required information.
+-- Group the table by title, then sort the count column in descending order.
+
+SELECT COUNT(title) AS title_count, title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY COUNT(title) DESC;
+
+-- Check table
+SELECT * FROM retiring_titles;
+
+-- Export the Retiring Titles table as retiring_titles.csv and
+    -- save it to your Data folder in the Pewlett-Hackard-Analysis folder.
+    
+---------------------------------------------------------------------------
+-- Write a query to create a Mentorship Eligibility table
+    -- that holds the employees who are eligible to participate in a mentorship program.
+
+-- Retrieve the emp_no, first_name, last_name, and birth_date columns from the Employees table.
+-- Retrieve the from_date and to_date columns from the Department Employee table.
+-- Retrieve the title column from the Titles table.
+-- Use a DISTINCT ON statement to retrieve the first occurrence of the employee number for each set of rows defined by the ON () clause.
+-- Create a new table using the INTO clause.
+-- Join the Employees and the Department Employee tables on the primary key.
+-- Join the Employees and the Titles tables on the primary key.
+-- Filter the data on the to_date column to all the current employees,
+    -- then filter the data on the birth_date columns to get all the employees whose birth dates
+    -- are between January 1, 1965 and December 31, 1965.
+-- Order the table by the employee number.
+
+SELECT DISTINCT ON (e.emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date, de.from_date, de.to_date, t.title
+INTO mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS de
+    ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS t
+    ON (e.emp_no = t.emp_no)
+WHERE (de.to_date = '9999-01-01')
+    AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
+
+-- Check table
+SELECT * FROM mentorship_eligibility;
+
+-- Export the Mentorship Eligibility table as mentorship_eligibilty.csv and
+    -- save it to your Data folder in the Pewlett-Hackard-Analysis folder.
--- retirement_queries.sql
+++ retirement_queries.sql
@@ -86,3 +86,108 @@ ORDER BY de.dept_no;
 
 -- Review dept_retirement_count table output
 SELECT * FROM dept_retirement_count
+
+-- Employee Information: A list of retirment eligible employees containing 
+    -- Unique employee number
+    -- Last name
+    -- First name
+    -- Gender
+    -- Salary
+
+SELECT e.emp_no, e.first_name, e.last_name, e.gender, s.salary
+INTO emp_info
+FROM employees AS e
+INNER JOIN salaries AS s
+    ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+    ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+    AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+    AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no;
+
+-- Check emp_info output
+SELECT * FROM emp_info;
+
+-- Management: A list of retirement eligible managers for each department, including:
+    -- Department number
+    -- Department name
+    -- Manager's employee number
+    -- Manager's last name
+    -- Manager's first name
+    -- Manager's starting employment date
+    -- Manager's ending employment date
+
+SELECT dm.dept_no, d.dept_name, ce.emp_no, ce.first_name, ce.last_name, dm.from_date, dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+INNER JOIN departments AS d
+    ON (dm.dept_no = d.dept_no)
+INNER JOIN current_emp AS ce
+    ON (dm.emp_no = ce.emp_no)
+ORDER BY dm.dept_no, dm.emp_no;
+
+-- Check emp_info output
+SELECT * FROM manager_info;
+
+-- Department Retirees: An updated current_emp list
+-- that includes everything it currently has
+-- but also includes the employee's departments
+    -- Employee's department
+    -- Employee's department name
+    -- Unique employee number
+    -- Last name
+    -- First name
+
+SELECT d.dept_no, d.dept_name, ce.emp_no, ce.first_name, ce.last_name
+INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+    ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+    ON (de.dept_no = d.dept_no)
+ORDER BY d.dept_no, ce.emp_no;
+
+-- Check dept_info output
+SELECT * FROM dept_info;
+
+-- Create a query that will return only the information relevant to the Sales team.
+-- The requested list includes:
+    -- Department name
+    -- Employee numbers
+    -- Employee first name
+    -- Employee last name
+
+SELECT d.dept_name, ce.emp_no, ce.first_name, ce.last_name
+INTO sales_info
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+    ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+    ON (de.dept_no = d.dept_no)
+WHERE (d.dept_name = 'Sales')
+ORDER BY ce.emp_no;
+
+-- Check dept_info output
+SELECT * FROM sales_info;
+
+-- Create a query that will return only the information relevant to the Sales and Development departments.
+-- The requested list includes:
+    -- Department name
+    -- Employee numbers
+    -- Employee first name
+    -- Employee last name
+
+SELECT d.dept_name, ce.emp_no, ce.first_name, ce.last_name
+-- INTO sales_development_info
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+    ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+    ON (de.dept_no = d.dept_no)
+WHERE d.dept_name IN ('Sales','Development')
+ORDER BY d.dept_name, ce.emp_no;
+
+-- Check dept_info output
+SELECT * FROM sales_development_info;
+
